b5ab662d400d8293493aa08d9321e16ecde9f691
phlabib <ph.labib@gmail.com>
2023-09-29 12:12:11 +0600

    register field createrd

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-
+import{NavLink} from "react-router-dom"
 
 const Header = () => {
 
-    const links = <>
-     <li><links to = '/'>Home</links></li>
-     <li><links to = '/login'>LogIn</links></li>
-     <li><links to = '/register'>Register</links></li>
+    const navLinks = <>
+    <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/register">Register</NavLink></li>
 
     </>
 
@@ -17,14 +17,14 @@ const Header = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-       {links}
+       {navLinks}
       </ul>
     </div>
     <a className="btn btn-ghost normal-case text-xl">Email Auth</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      {links}
+      {navLinks}
     </ul>
   </div>
 </div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,24 @@
 
 
 const Register = () => {
+
+    const handleRegister = e => {
+        e.preventDegault ();
+        console.log('object');
+
+    }
+
+
     return (
-        <div>
-            <h2 className="text-3xl">Please Register</h2>
+        <div className="">
+           <div className="mx-auto md:w-1/2  text-center">
+           <h2 className="text-3xl mb-4 text-center">Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input className="border mb-4 w-3/4 py-2 px-4" type="email" name="email" placeholder="E-mail"/> <br />
+                <input className="border mb-4 w-3/4 py-2 px-4" type="password" name="password"  placeholder="Password"/> <br />
+                <input className="btn btn-secondary mb-4 w-3/4" type="submit" value='Register' />
+            </form>
+           </div>
         </div>
     );
 };
